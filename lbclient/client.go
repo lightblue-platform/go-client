19077c67f9086c9a3afa7ac9b2c21b674e6709d7
Burak Serdar <bserdar@redhat.com>
2017-03-22 12:47:08 -0600

    Split interfaces

--- lbclient/client.go
+++ lbclient/client.go
@@ -2,10 +2,17 @@ package lbclient
 
 import ()
 
-type LBClient interface {
-	Find(request *FindRequest, data interface{}) (*Response, error)
-	Insert(request *InsertRequest, data interface{}) (*Response, error)
-	Update(request *UpdateRequest, data interface{}) (*Response, error)
-	Save(request *SaveRequest, data interface{}) (*Response, error)
+type DataServiceClient interface {
+	Find(request *FindRequest, returndata interface{}) (*Response, error)
+	Insert(request *InsertRequest, returndata interface{}) (*Response, error)
+	Update(request *UpdateRequest, returndata interface{}) (*Response, error)
+	Save(request *SaveRequest, returndata interface{}) (*Response, error)
 	Delete(request *DeleteRequest) (*Response, error)
+}
+
+type LockingClient interface {
+	Acquire(domain, callerId, resourceId string, ttl int) (bool, error)
+	Release(domain, callerId, resourceId string) (bool, error)
+	GetLockCount(domain, callerId, resourceId string) (int, error)
+	Ping(domain, callerId, resourceId string) (bool, error)
 }
